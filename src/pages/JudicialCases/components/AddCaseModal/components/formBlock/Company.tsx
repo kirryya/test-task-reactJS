9d3e7d831e83ea2274e6178316f5786594591f66
kirryya <kir1987@inbox.ru>
2022-09-22 22:35:47 +0300

    refactoring

--- src/pages/JudicialCases/components/AddCaseModal/components/formBlock/Company.tsx
+++ src/pages/JudicialCases/components/AddCaseModal/components/formBlock/Company.tsx
@@ -1,72 +1,64 @@
 import React, {FC} from 'react';
 import style from '../header/styles/Header.module.css';
-import {Field} from "formik";
+import { Field } from "formik";
 import { PurposeType } from './types';
+import { ReturnComponentType } from "../../types";
 
-export const Company: FC<PurposeType> = ({ title }: PurposeType) => {
+export const Company: FC<PurposeType> = ({ title }: PurposeType): ReturnComponentType => {
     return (
         <div>
-            <label className={style.value} htmlFor={`${title}.inn`}>
+            <label className={style.value} >
                 ИНН
                 <Field type='text' name={`${title}.inn`} />
             </label>
-            <label className={style.value} htmlFor='plaintiff.ogrn'>
+            <label className={style.value} >
                 ОГРН
-                <Field type='text' name='plaintiff.ogrn' />
+                <Field type='text' name={`${title}.ogrn`} />
             </label>
-            <label className={style.value} htmlFor='plaintiff.company'>
+            <label className={style.value} >
                 Название компании
-                <Field type='text' name='plaintiff.company' />
+                <Field type='text' name={`${title}.company`} />
             </label>
-            <label
-                className={style.value}
-                htmlFor='plaintiff.legal_address'
-            >
+            <label className={style.value} >
                 Юридический адрес
                 <Field
                     type='text'
-                    name='plaintiff.legal_address'
+                    name={`${title}.legal_address`}
                     className={style.field}
                 />
             </label>
-            <label
-                className={style.value}
-                htmlFor='plaintiff.legalIsMailing'
-            >
-                <Field type='checkbox' name='plaintiff.legalIsMailing' />
+            <label className={style.value}>
+                <Field type='checkbox' name={`${title}.legalIsMailing`} />
                 Почтовый адрес совпадает
                 <br />с юридическим
             </label>
-            <label
-                className={style.value}
-                htmlFor='plaintiff.mailing_address'
-            >
+            <label className={style.value} >
                 Почтовый адрес
                 <Field
                     type='text'
-                    name='plaintiff.mailing_address'
+                    name={`${title}.mailing_address`}
                     className={style.field}
                 />
             </label>
-            <label className={style.value} htmlFor='plaintiff.kpp'>
+            <label className={style.value}>
                 КПП
                 <Field type='text' name='plaintiff.kpp' />
             </label>
-            <label className={style.value} htmlFor='plaintiff.pc'>
+            <label className={style.value}>
                 РС
-                <Field type='text' id='plaintiff.pc' name='plaintiff.pc' />
+                <Field type='text' name={`${title}.pc`} />
             </label>
-            <label className={style.value} htmlFor='plaintiff.bank'>
+            <label className={style.value}>
                 Банк
-                <Field type='text' name='plaintiff.bank' />
+                <Field type='text' name={`${title}.bank`} />
             </label>
-            <label className={style.value} htmlFor='plaintiff.bic'>
+            <label className={style.value}>
                 БИК
-                <Field type='text' name='plaintiff.bic' />
+                <Field type='text' name={`${title}.bic`} />
             </label>
-            <label className={style.value} htmlFor='plaintiff.kc'>
+            <label className={style.value}>
                 К/с
-                <Field type='text' name='plaintiff.kc' />
+                <Field type='text' name={`${title}.kc`} />
             </label>
             </div>
     );
--- src/pages/JudicialCases/components/AddCaseModal/components/formBlock/People.tsx
+++ src/pages/JudicialCases/components/AddCaseModal/components/formBlock/People.tsx
@@ -2,51 +2,46 @@ import React, {FC} from 'react';
 import style from '../header/styles/Header.module.css';
 import {Field} from "formik";
 import { PurposeType } from './types';
+import { ReturnComponentType } from "../../types";
 
-export const People: FC<PurposeType> = ( { title }: PurposeType ) => {
+export const People: FC<PurposeType> = ( { title }: PurposeType ): ReturnComponentType => {
     return (
         <div>
-            <label className={style.value} htmlFor={`${title}.lastname`}>
+            <label className={style.value}>
                 Фамилия
-                <Field type='text' name='plaintiff.lastname' />
+                <Field type='text' name={`${title}.lastname`} />
             </label>
-            <label className={style.value} htmlFor='plaintiff.name'>
+            <label className={style.value}>
                 Имя
-                <Field type='text' name='plaintiff.name' />
+                <Field type='text' name={`${title}.name`} />
             </label>
-            <label className={style.value} htmlFor='plaintiff.surname'>
+            <label className={style.value}>
                 Отчество
-                <Field type='text' name='plaintiff.surname' />
+                <Field type='text' name={`${title}.surname`}/>
             </label>
-            <label className={style.value} htmlFor='plaintiff.birthday'>
+            <label className={style.value}>
                 Дата рождения
-                <Field type='date' name='plaintiff.birthday' />
+                <Field type='date' name={`${title}.birthday`} />
             </label>
-            <label
-                className={style.value}
-                htmlFor='plaintiff.reg_address'
-            >
+            <label className={style.value} >
                 Адрес регистрации
                 <Field
                     type='text'
-                    name='plaintiff.reg_address'
+                    name={`${title}.reg_address`}
                     className={style.field}
                 />
             </label>
-            <label
-                className={style.value}
-                htmlFor='plaintiff.res_address'
-            >
+            <label className={style.value}>
                 Адрес проживания
                 <Field
                     type='text'
-                    name='plaintiff.res_address'
+                    name={`${title}.res_address`}
                     className={style.field}
                 />
             </label>
-            <label className={style.value} htmlFor='plaintiff.phone'>
+            <label className={style.value}>
                 Телефон
-                <Field type='text' name='plaintiff.phone' />
+                <Field type='text' name={`${title}.phone`} />
             </label>
             </div>
     );
